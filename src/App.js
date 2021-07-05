dbb4aae6d0169965a92b0b00fc5b96b939d1d0b6
chetan <taydechetan983@gmail.com>
2021-07-05 22:47:13 +0530

    my first commit reactwebsite

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React ,{Component} from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import { robot } from './Robot';
+import './App.css';
+import Scroll from './Scroll';
+
+
+class App extends Component{
+    constructor(){
+        super()
+        this.state={
+            robot:robot,
+            searchfield:''
+        }
+    }
+    onSearchChange=(event) =>{
+        this.setState({searchfield: event.target.value})
+    }
+    render(){
+        const filteredRobot = this.state.robot.filter(robot=>{
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())} )
+        return (
+            <div className='tc'>
+                <h1 className='f1'>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <Scroll>
+                <CardList robot={filteredRobot} />
+                </Scroll>
+            </div>
+        );
+    }
+    }
+
+export default App;
---
+++ src/Robot.js
@@ -0,0 +1,62 @@
+export const robot = [
+    {
+        id: 1,
+        name: 'Chetan',
+        username: 'chat',
+        email: 'chetan@gmail.biz'
+    },
+    {
+        id: 2,
+        name: 'Vishal',
+        username: 'vish',
+        email: 'vishal@gmail.biz'
+    },
+    {
+        id: 3,
+        name: 'Roshan',
+        username: 'roo',
+        email: 'roshan@gmail.biz'
+    },
+    {
+        id: 4,
+        name: 'Shubham',
+        username: 'shubh',
+        email: 'shubham@gmail.biz'
+    },
+    {
+        id: 5,
+        name: 'Mazhar',
+        username: 'mz',
+        email: 'mazhar@gmail.biz'
+    },
+    {
+        id: 6,
+        name: 'Krishan',
+        username: 'krish',
+        email: 'krishan@gmail.biz'
+    },
+    {
+        id: 7,
+        name: 'Himanshu',
+        username: 'himmy',
+        email: 'himanshu@gmail.biz'
+    },
+    {
+        id: 8,
+        name: 'Shad',
+        username: 'shadu',
+        email: 'shhad@gmail.biz'
+    },
+    {
+        id: 9,
+        name: 'Mohit',
+        username: 'moh',
+        email: 'mohit@gmail.biz'
+    },
+    {
+        id: 10,
+        name: 'Sahil',
+        username: 'darkdevil',
+        email: 'sahil@gmail.biz'
+    },
+]
